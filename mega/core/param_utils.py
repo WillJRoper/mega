d0e068c1c84fbc132ddce81ef07066ec644c0e6c
wjr21 <wjr21@sussex.ac.uk>
2022-04-20 14:14:41 +0100

    Now deriving mean density from cosmological quantities

--- mega/core/param_utils.py
+++ mega/core/param_utils.py
@@ -60,6 +60,7 @@ class Metadata:
         self.findsubs = flags["find_subs"]
         self.profile = flags["profile"]
         self.dmo = flags["DMO"]
+        self.with_hydro = not self.dmo
         self.debug = flags["debug_mode"]
 
         # MPI information (given value if needed)
@@ -70,30 +71,37 @@ class Metadata:
         # Open hdf5 file
         self.snap = snaplist[snap_ind]
         if boxsize is None:
+            # TODO: Replace this with a function for reading metadata
             hdf = h5py.File(self.inputpath + self.snap + ".hdf5", "r")
             self.boxsize = hdf["Header"].attrs["BoxSize"]
             self.npart = hdf["Header"].attrs["NumPart_Total"]
             self.z = hdf["Header"].attrs["Redshift"]
-            self.tot_mass = np.sum(hdf["PartType1/Masses"]) * 10 ** 10
             hdf.close()
         else:
             self.boxsize = boxsize
             self.npart = npart
             self.z = z
-            self.tot_mass = tot_mass
         self.periodic = simulation["periodic"]
         self.box_vol = self.boxsize[0] * self.boxsize[1] * self.boxsize[2]
-        self.mean_sep = (self.box_vol / self.npart[1]) ** (1. / 3.)
         self.comoving_soft = simulation["comoving_DM_softening"]
         self.max_physical_soft = simulation["max_physical_DM_softening"]
-        mean_den = (self.tot_mass * u.M_sun / self.box_vol
-                    / u.Mpc ** 3 * (1 + self.z) ** 3)
-        self.mean_den = mean_den.to(u.M_sun / u.km ** 3)
         self.a = 1 / (1 + self.z)
         if self.comoving_soft * self.a > self.max_physical_soft:
             self.soft = self.max_physical_soft / self.a
         else:
             self.soft = self.comoving_soft
+
+        # Calculate mean separations
+        self.mean_sep = np.zeros(len(self.npart), dtype=np.float64)
+        for i in range(len(self.npart)):
+            if self.npart[i] > 0:
+                if i == 1:
+                    self.mean_sep[i] = (self.box_vol
+                                        / self.npart[i]) ** (1. / 3.)
+                else:
+                    self.mean_sep[i] = (self.box_vol
+                                        / (np.sum(self.npart)
+                                           - self.npart[1])) ** (1. / 3.)
 
         # Let"s get the progenitor and descendent snapshots
         self.prog_snap = None
@@ -119,18 +127,43 @@ class Metadata:
                                    Tcmb0=cosmology["Tcmb0"],
                                    Ob0=cosmology["Ob0"])
 
+        # Extract the mean density
+        self.crit_density = self.cosmo.critical_density(self.z)
+        self.omega_m = self.cosmo.Om(self.z)
+        self.mean_den = (self.omega_m * self.crit_density).to(u.M_sun
+                                                              / u.km ** 3)
+
         # Debugging gubbins
         if self.debug:  # set all snapshots as the same
             self.prog_snap = snaplist[snap_ind]
             self.desc_snap = snaplist[snap_ind]
 
         # If were running in DMO mode lets ensure we remove all particles
+        temp_npart = np.zeros(len(self.npart), dtype=int)
         if self.dmo:
-            temp_npart = np.zeros(len(self.npart), dtype=int)
             temp_npart[1] = self.npart[1]
             self.npart = temp_npart
+        else:
+            # Always ignore boundary particles
+            for part_type in range(len(self.npart)):
+                if part_type in [2, 3] \
+                        or part_type >= simulation["ignored_species_lim"]:
+                    continue
+                temp_npart[part_type] = self.npart[part_type]
+            self.npart = temp_npart
+
+        # Define list of particle types present
         self.part_types = [1, ] + [i for i in range(len(self.npart))
                                    if self.npart[i] != 0 and i != 1]
+
+        # Define particle index offsets
+        self.part_ind_offset = np.array([0, ] * len(self.npart), dtype=int)
+        offset = 0
+        for i in self.part_types:
+            self.part_ind_offset[i] = offset
+            offset += self.npart[i]
+        self.hydro_ind_offset = self.part_ind_offset - self.npart[1]
+        self.hydro_ind_offset[1] = 0
 
         # Find the index offsets for each particle type
         offset = self.npart[1]
@@ -158,8 +191,8 @@ class Metadata:
         self.vlinkl_indp = (np.sqrt(self.G / 2)
                             * (4 * np.pi * 200
                                * self.mean_den / 3) ** (1 / 6)).value
-        self.sub_vlinkl_indp = self.vlinkl_indp * (self.linkl
-                                                   / self.sub_linkl) ** (1 / 2)
+        self.sub_vlinkl_indp = self.vlinkl_indp \
+                               * (self.linkl[1] / self.sub_linkl[1]) ** (1 / 2)
 
         # Halo
         self.part_thresh = params["part_threshold"]
