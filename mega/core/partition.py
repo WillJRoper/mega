d53e3c62863d59c7b6c12810e37001823c0a9ad9
wjr21 <wjr21@sussex.ac.uk>
2022-06-22 00:22:16 +0100

    Improved asserts in partition

--- mega/core/partition.py
+++ mega/core/partition.py
@@ -79,6 +79,6 @@ def stripe_cells(meta):
     # Ensure all cells have been partioned
     assert np.min(cell_ranks) >= 0, "Not all cells assigned to a rank"
     assert np.max(
-        cell_ranks) > nranks, "Cells sorted into too many ranks: %d but should be %d" % (np.max(cell_ranks), nranks - 1)
+        cell_ranks) == nranks - 1, "Cells sorted into too many ranks: %d but should be %d" % (np.max(cell_ranks), nranks - 1)
 
     return cell_ranks, cell_rank_dict
